f01af6635f11be5c3efed9a67caf9dca9370d20e
LeavaTail <starbow.duster@gmail.com>
2020-06-27 16:12:00 +0900

    Revert "[Mod] Output debug message in Travis CI"
    This reverts commit ede5b3f959f11e94c0133035cb1e1bed4aec09dc.

--- tests/default_run.sh
+++ tests/default_run.sh
@@ -10,9 +10,7 @@ FILESIZE=1048576
 
 ### main function
 strace -o trace.log ./rwtest
-cat trace.log
-./tests/trace_chunk.awk -v FILE=${TARGETFILE} -v CHUNKSIZE=${CHUNKSIZE} -v FILESIZE=${FILESIZE} trace.log
+./tests/trace_chunk.awk -v FILE=${TARGETFILE} -v CHUNKSIZE=${CHUNKSIZE} -v FILESIZE=${FILESIZE} trace.log || cat trace.log
 
 ls -l ${TARGETDIR} > trace2.log
-cat trace2.log
-./tests/trace_filesize.awk -v FILE=${TARGETFILE} -v CHUNKSIZE=${CHUNKSIZE} -v FILESIZE=${FILESIZE} trace2.log
+./tests/trace_filesize.awk -v FILE=${TARGETFILE} -v CHUNKSIZE=${CHUNKSIZE} -v FILESIZE=${FILESIZE} trace2.log || cat trace2.log
